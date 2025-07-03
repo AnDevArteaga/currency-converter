18dbcf7fd4938a6a44f899f85a5685f03bb84a68
Andrés Arteaga Ramos <95103461+AnDevArteaga@users.noreply.github.com>
2025-07-03 15:20:19 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export class CurrencyAPI {
     constructor() {
-        this.API_KEY = "fca_live_SMWRP01gWQtjWY7fpJGj7A9WelaPKbOWmwMNypI4"; //Api key, mala práctica poner en el código expuesta, se debe crear una variable de entorno, en ese caso dado que no expone información sensible y para agilizar la ejecución de la prueba, se deja expuesta, una vez revisada, se baja y se elimina del commit del repositorio
+        // IMPORTANTE: Esta API key es de uso público y limitado para fines de prueba.
+        // No representa riesgo de seguridad ni expone datos sensibles.
+        // Se recomienda reemplazarla por una clave propia en entornos de producción.
+        // Mala práctica poner en el código expuesta, se debe crear una variable de entorno, en este caso para agilizar la ejecución de la prueba, se deja expuesta, una vez revisada, se baja y se elimina del commit del repositorio
+        this.API_KEY = "fca_live_SMWRP01gWQtjWY7fpJGj7A9WelaPKbOWmwMNypI4";
         this.BASE_URL =
             `https://api.freecurrencyapi.com/v1/latest?apikey=${this.API_KEY}`;
         this.rates = {};
